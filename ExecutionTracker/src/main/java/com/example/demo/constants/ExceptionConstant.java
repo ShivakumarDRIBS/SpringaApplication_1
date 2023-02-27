328b1bac7c5c944a2a0f9ef318b84b702fb7ed69
2210867 <2210867@LTIN408917.cts.com>
2023-02-27 17:42:07 +0530

    011 change

--- ExecutionTracker/src/main/java/com/example/demo/constants/ExceptionConstant.java
+++ ExecutionTracker/src/main/java/com/example/demo/constants/ExceptionConstant.java
@@ -3,5 +3,5 @@ package com.example.demo.constants;
 public class ExceptionConstant {
 
 	public static final String error="ERROR";
-	public static final String error_789="ERROR";
+	public static final String error_011="ERROR";
 }
